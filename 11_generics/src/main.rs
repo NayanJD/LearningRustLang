43d558fc2617dbd3c60e69b89355fc0bbbd014ee
Nayan Das <dastms@gmail.com>
2025-04-21 07:34:47 +0000

    Add lifetimes

--- 11_generics/src/main.rs
+++ 11_generics/src/main.rs
@@ -48,6 +48,32 @@ fn main() {
     // importing Summary trait is important to use summarize() on the type
     // Tweet
     println!("1 new tweet: {}", tweet.summarize());
+
+    let r;
+
+    {
+        let x = 5;
+        r = &x;
+    }
+    // x dropped here hence r cannot be used
+    //
+    // println!("r: {r}")
+
+    let string1 = String::from("long string is long");
+
+    {
+        let string2 = String::from("xyz");
+
+        let result = longest(&string1, &string2);
+
+        println!("The longer string is {result}")
+    }
+
+    // This line is not valid because in the definition of longest function the returned value has
+    // a lifetime of smaller among the provided arguments. Here, string2 has smaller lifetime and
+    // hence result also has the same lifetime as string2.
+    //
+    // println!("The longer string is {result}")
 }
 
 fn largest(list: &[i32]) -> &i32 {
@@ -72,4 +98,33 @@ fn largest_gen<T>(list: &[T]) -> &T {
     }
 
     return largest;
+}
+
+fn notify(item: &impl Summary) {
+    println!("Breaking news! {}", item.summarize())
+}
+
+fn notify_traitbounds<T: Summary>(item: T) {
+    println!("Breaking news! {}", item.summarize())
+}
+
+// The section https://doc.rust-lang.org/book/ch10-02-traits.html#specifying-multiple-trait-bounds-with-the--syntax
+// is not put into code here.
+
+// This function wont work because compiler does not know which of x or y is being returned. This
+// does not pass borrow checker until lifetime is defined
+// fn longest(x: &str, y: &str) -> &str {
+//     if x.len() > y.len() {
+//         x
+//     } else {
+//         y
+//     }
+// }
+
+fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
 }
