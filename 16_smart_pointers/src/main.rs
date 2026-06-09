f9cdee96eec88c700101ae70f273476d86b5328a
Nayan Das <ndas@digitalocean.com>
2026-06-09 19:16:05 +0530

    Add ref count

--- 16_smart_pointers/src/main.rs
+++ 16_smart_pointers/src/main.rs
@@ -53,8 +53,16 @@ fn main() {
 
     println!("CustomSmartPointer dropped before the end of main");
 
-    let g = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
-    let h = Cons(3, Rc::clone(&g));
+    let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
+    println!("count after creating a = {}", Rc::strong_count(&a));
+    let b = Cons(3, Rc::clone(&a));
+    println!("count after creating b = {}", Rc::strong_count(&a));
+
+    {
+        let c = Cons(4, Rc::clone(&a));
+        println!("count after creating c = {}", Rc::strong_count(&a));
+    }
+    println!("count after c got out of scope = {}", Rc::strong_count(&a));
 }
 
 struct MyBox<T>(T);
