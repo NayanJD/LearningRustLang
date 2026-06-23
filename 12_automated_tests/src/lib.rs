61262527ac4079dfa8478965505131708ce76fbe
Nayan Das <ndas@digitalocean.com>
2026-06-23 10:51:27 +0530

    Add Guess example in automated tests

--- 12_automated_tests/src/lib.rs
+++ 12_automated_tests/src/lib.rs
@@ -127,3 +127,32 @@ mod tests_silly_function {
         assert_eq!(value, 5);
     }
 }
+
+pub struct Guess {
+    value: i32,
+}
+
+impl Guess {
+    pub fn new(value: i32) -> Guess {
+        if value < 1 {
+            panic!("Guess must be greater than or equal to 1, got {value}")
+        }
+
+        if value > 100 {
+            panic!("Guess must be less than or equal to 100, got {value}")
+        }
+
+        Guess { value }
+    }
+}
+
+#[cfg(test)]
+mod tests_guess {
+    use super::*;
+
+    #[test]
+    #[should_panic(expected = "less than or equal to 100")]
+    fn greater_than_100() {
+        Guess::new(200);
+    }
+}
