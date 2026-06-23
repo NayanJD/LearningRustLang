8eda36bda75ec8b370e3d073ce255a141502cb5e
Nayan Das <ndas@digitalocean.com>
2026-06-23 11:20:48 +0530

    Complete automated tests

--- 12__automated_tests/tests/integration_test.rs
+++ 12__automated_tests/tests/integration_test.rs
@@ -1,8 +1,12 @@
 use addr::add;
 
+mod common;
+
 // this can be run with cargo test --test integration_test
 #[test]
 fn it_adds_two() {
+    common::setup();
+
     let result = add(2, 2);
     assert_eq!(result, 4);
 }
